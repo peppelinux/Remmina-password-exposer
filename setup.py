e8f20faedb4c75d293d97177612bdd08c98676f2
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-02-04 10:43:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='remmina-password-exposer',
+      version='0.3',
+      description="Print remmina password in clear",
+      long_description=readme(),
+      classifiers=['Development Status :: 5 - Production/Stable',
+                  'License :: OSI Approved :: BSD License',
+                  'Programming Language :: Python :: 2',
+                  'Programming Language :: Python :: 3'],
+      url='https://github.com/peppelinux/Remmina-password-exposer',
+      author='Giuseppe De Marco',
+      author_email='giuseppe.demarco@unical.it',
+      license='BSD',
+      packages=['remmina-password-exposer'],
+      install_requires=[
+                      'pycrypto'
+                  ],
+     )
